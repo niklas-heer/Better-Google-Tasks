c45fa38230835ecdd76034737d04ff06aff2bca6
Niklas Heer <niklas.heer@me.com>
2014-04-06 04:59:15 +0200

    changed paths

--- src/js/background.js
+++ src/js/background.js
@@ -1,43 +1,31 @@
-$( document ).ready( function() {
+(function() {
+  $(document).ready(function() {
+    var address, frame;
+    localStorage.removeItem("com.bit51.chrome.bettergoogletasks.last_notify");
+    address = "https://mail.google.com/tasks/m";
+    frame = document.createElement("iframe");
+    frame.setAttribute("src", address);
+    frame.setAttribute("id", "tasksPage");
+    document.getElementById("content").appendChild(frame);
+  });
 
-	localStorage.removeItem( 'com.bit51.chrome.bettergoogletasks.last_notify' );
+  updateData();
 
-	address = 'https://mail.google.com/tasks/m';
+  getManifest(function(manifest) {
+    localStorage.setItem("com.bit51.chrome.bettergoogletasks.version", manifest.version);
+  });
 
-	var frame = document.createElement( 'iframe' );
+  chrome.extension.onConnect.addListener(function(port) {
+    console.assert(port.name === "BGT");
+    port.onMessage.addListener(function(msg) {
+      if (msg.message === "Update") {
+        updateData();
+      } else {
+        if (msg.message === "Open") {
+          inOpen();
+        }
+      }
+    });
+  });
 
-	frame.setAttribute( 'src', address );
-	frame.setAttribute( 'id', 'tasksPage' );
-	document.getElementById( 'content' ).appendChild( frame );
-
-} );
-
-//Initialize the extension
-updateData();
-
-
-//Set the extension version
-getManifest( function( manifest ) {
-	localStorage.setItem( 'com.bit51.chrome.bettergoogletasks.version', manifest.version );
-} );
-
-//Set up the appropriate listeners
-chrome.extension.onConnect.addListener( function( port ) {
-
-	console.assert( port.name == "BGT" );
-
-	port.onMessage.addListener( function( msg ) {
-
-		if ( msg.message == "Update" ) {
-
-			updateData();
-
-		} else if ( msg.message == "Open" ) {
-
-			inOpen();
-
-		}
-
-	} );
-
-} );
+}).call(this);
